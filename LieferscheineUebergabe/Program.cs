4bcabbde89cc8484ae9cb1712fceeaf2b267a185
Kratoleg <info@olegz.de>
2024-10-01 21:43:18 +0200

    Add project files.

---
+++ LieferscheineUebergabe/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+using MontageScanLib;
+using System.ComponentModel;
+
+
+BindingList<MontageLieferscheinModel> angezeigteLieferscheine = new BindingList<MontageLieferscheinModel>();
+
+CsvManager.CreateCsvFile();
+
+
+CsvManager.FillListWithLastEntrys(angezeigteLieferscheine, 100);
+
